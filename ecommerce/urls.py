a78207b67e05870711f66c5e55bd435551facefd
ismherram <ismherram@alum.us.es>
2023-12-02 17:36:12 +0100

    Vuelta a la base de datos y redireccion a store automatica

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='store/', permanent=True)),
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
     path('users/', include('users.urls')),
