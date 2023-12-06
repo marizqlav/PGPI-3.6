e7f24742d4362b58945ca65cd9f3f3da8e0aeb72
ismherram <ismherram@alum.us.es>
2023-12-06 16:40:12 +0100

    Modulo de opiniones terminados

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from . import views
+from .views import review_list_error,add_review, review_list, edit_review, delete_review
+
 
 urlpatterns = [
-    path('reviews/', views.review_list, name='review_list'),
-    path('add_review/', views.add_review, name='add_review'),
-    path('reviews/reviews/add_review/', views.add_review, name='add_review'),
-    
+    path('reviews/', review_list, name='review_list'),
+    path('add_review/', add_review, name='add_review'),
+    path('reviews/reviews/add_review/', add_review, name='add_review'),
+    path('edit_review/<int:review_id>/', edit_review, name='edit_review'),
+    path('delete_review/<int:review_id>/', delete_review, name='delete_review'),
+    path('review_list_error/', review_list_error, name='review_list_error'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,19 +1,72 @@
 from django.shortcuts import render, redirect
 from .models import Review
+from django.contrib import messages
+from store.utils import cookieCart, cartData, guestOrder
+from django.shortcuts import get_object_or_404
+
 from .forms import ReviewForm
 
 def review_list(request):
     reviews = Review.objects.all().order_by('-created_at')
-    return render(request, 'review_list.html', {'reviews': reviews})
+    data = cartData(request)
+    if request.user.is_authenticated:
+        user_has_reviewed = Review.objects.filter(user=request.user).exists()
+    else:
+        user_has_reviewed = False
+    cartItems = data['cartItems']
+    return render(request, 'review_list.html', {'reviews': reviews, 'cartItems': cartItems,'user_has_reviewed': user_has_reviewed})
 
 def add_review(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
-            review = form.save(commit=False)
-            review.user = request.user  # Assign the current logged-in user
-            review.save()
-            return redirect('review_list')
+            if Review.objects.filter(user=request.user).exists():
+                messages.error(request, 'Ya tenías una opinión.')
+                return redirect('review_list_error')
+            else:
+                review = form.save(commit=False)
+                review.user = request.user  # Assign the current logged-in user
+                review.save()
+                return redirect('review_list')
     else:
         form = ReviewForm()
-    return render(request, 'add_review.html', {'form': form})
+        
+    return render(request, 'add_review.html', {'form': form,'cartItems':cartItems})
+
+def review_list_error(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
+    return render(request, 'review_list_error.html',{'cartItems':cartItems})
+
+def edit_review(request, review_id):
+    data = cartData(request)
+    cartItems = data['cartItems']
+    review = get_object_or_404(Review, id=review_id)
+    if request.user != review.user:
+        messages.error(request, 'No tienes permiso para editar esta opinión.')
+        return redirect('review_list')
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            return redirect('review_list')
+    else:
+        form = ReviewForm(instance=review)
+    return render(request, 'edit_review.html', {'form': form,'cartItems':cartItems})
+
+def delete_review(request, review_id):
+    data = cartData(request)
+    cartItems = data['cartItems']
+    review = get_object_or_404(Review, id=review_id)
+    if request.user != review.user:
+        messages.error(request, 'No tiene permiso para eliminar esta opinión.')
+        return redirect('review_list')
+
+    if request.method == 'POST':
+        review.delete()
+        return redirect('review_list')
+
+    return render(request, 'confirm_delete.html', {'object': review,'cartItems':cartItems})
